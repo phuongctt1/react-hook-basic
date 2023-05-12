b165537e40079fc0e41e33d865823be564af2a16
your name <phuong.caothithu1@gmail.com>
2023-05-12 17:13:36 +0700

    du lieu cua bien

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  let text = "phuong"; //string
+  let number = 2023;
+  //let boolean = true;
+  let obj = {name:'phuong', hocreactjs:'Hoc reactjs hook'};
+  let link = 'https://www.youtube.com/watch?v=MsyZGJ4aoGs&list=PLMbuMydSxMKxfg0OeJJoNsdRTnTIEcqmg'
   return (
     <div className="App">
+      {console.log('check obj: ',obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with react js!!</h1>
+        <h1>Hello world with react js {text} and  in {number} !!</h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target = "_blank">visit youtube</a>
       </header>
     </div>
   );
