feede7c796bec1da217bf003a2802c4bd747d021
your name <phuong.caothithu1@gmail.com>
2023-05-16 22:52:14 +0700

    onclick

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import React from "react";
+
 const TodoList = () => {
-    const [name, setName] = useState("")
+    const [name, setName] = useState("");
+    const handleClickButton = (event,msg) => {
+        console.log(">>run inside handleClickButton---:", name )
+    
+    };
+    const handleInputChange = (event) => {
+        setName(event.target.value);
+    };
     return(
         <div>
             <label>Name</label>
-            <input value={name} type = "text"
-            onChange= {(event) => {
-                console.log("check event: ", event)
-                setName(event.target.value)
-                } } />
+           
+            <input value={name} type="text" onChange={handleInputChange} />
+            <button type='button' onClick={ (event) => handleClickButton(event,'buttonClick')}> Submit</button>
             <br/><br/>
             Hello study with me {name}
         </div>
