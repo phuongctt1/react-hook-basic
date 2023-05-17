b6e266142270e4486aa7613ec2692ddd3637611a
your name <phuong.caothithu1@gmail.com>
2023-05-17 14:41:09 +0700

    array

---
+++ src/components/Todolist.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import React from "react";
+//array
+const TodoList = () => {
+    const [name, setName] = useState("");
+    const [listTodo, setListTodo] = useState(
+        
+    [
+       { id: 'todo1', name: "whatching youtube"},
+       { id: 'todo2', name: "using facebook"},
+       { id: 'todo3', name: "Reading book"},
+    ]
+    )
+    console.log('>>>> check data:', listTodo)
+
+    const handleClickButton = (event,msg) => {
+        console.log(">>run inside handleClickButton---:", name )
+    
+    };
+    const handleInputChange = (event) => {
+        setName(event.target.value);
+    };
+    return(
+        <div>
+            <label>Name</label>
+           
+            <input value={name} type="text" onChange={handleInputChange} />
+            <button type='button' onClick={ (event) => handleClickButton(event,'buttonClick')}> Submit</button>
+            <br/><br/>
+            Hello study with me {name}
+        </div>
+    );
+}
+
+export default TodoList;
